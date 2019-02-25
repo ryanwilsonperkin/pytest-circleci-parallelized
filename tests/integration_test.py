96b394319872ff194adcff89cbf5e5d9c6a80344
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2019-02-24 20:08:33 -0500

    Integration test for use in CircleCI

---
+++ tests/integration_test.py
@@ -0,0 +1,37 @@
+def test_with_circleci_parallelize(testdir):
+    testdir.makepyfile(
+        test_fast_slow_classes="""
+import unittest
+
+class SlowTestCase(unittest.TestCase):
+    def test_something(self):
+        time.sleep(5)
+        assert True
+
+class FastTestCase1(unittest.TestCase):
+    def test_something(self):
+        time.sleep(1)
+        assert True
+
+class FastTestCase2(unittest.TestCase)
+    def test_something(self):
+        time.sleep(1)
+        """
+    )
+
+    result = testdir.runpytest("-v", "--circleci-parallelize")
+    if "running 1 items" in result.stdout.str():
+        # If the node only ran one item it should be the slow one
+        result.stdout.fnmatch_lines(["*SlowTestCase::test_something PASSED*"])
+    elif "running 2 items" in result.stdout.str():
+        # If the node ran two items it should be the fast ones
+        result.stdout.fnmatch_lines(
+            [
+                "*FastTestCase1::test_something PASSED*"
+                "*FastTestCase2::test_something PASSED*"
+            ]
+        )
+    else:
+        assert False, "Test splitting should cause only 1 or 2 items to be run."
+
+    assert result.ret == 0
