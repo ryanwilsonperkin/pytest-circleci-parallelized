d9989373d30ab94c6cc2a09495ce9dce3f6f2595
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2022-10-20 16:14:02 -0400

    v0.1.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(file_name):
 
 setuptools.setup(
     name="pytest-circleci-parallelized",
-    version="0.0.4",
+    version="0.1.0",
     author="Ryan Wilson-Perkin",
     author_email="ryanwilsonperkin@gmail.com",
     license="MIT",
